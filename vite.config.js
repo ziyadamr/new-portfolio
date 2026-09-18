d434ba27dc188f8998a82f6630b2f86a9008a83f
Ziyad Amr <75838055+ziyadamr@users.noreply.github.com>
2026-09-18 17:44:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/new-portfolio/', // 👈 اكتب هنا نفس اسم الريبو على GitHub بالمللي بين سلاشين
+  base: '/', 
 })
